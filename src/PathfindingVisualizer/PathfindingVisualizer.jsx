894bc9e3980a7f2af4bf6b15a2846ed38ac49866
andrewywong <ndrew.wong@gmail.com>
2020-06-10 17:16:15 -0500

    Change board display to look nicer

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -7,16 +7,15 @@ export default class PathfindingVisualizer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nodes: []
+      nodes: [],
     };
   }
 
   componentDidMount() {
     const nodes = [];
     // Change rows and cols depending on the device width
-    let maxCols = window.innerWidth / 25;
-    maxCols = Math.round(maxCols * 0.8);
-    let maxRows = Math.round(maxCols / 2);
+    let maxCols = window.innerWidth / 28;
+    let maxRows = Math.round(maxCols / 3);
     console.log(maxRows);
 
     for (let row = 0; row < maxRows; ++row) {
@@ -30,13 +29,16 @@ export default class PathfindingVisualizer extends Component {
             col === Math.trunc(maxCols * (1 / 4)),
           isFinish:
             row === Math.trunc(maxRows / 2) &&
-            col === Math.trunc(maxCols * (3 / 4))
+            col === Math.trunc(maxCols * (3 / 4)),
         };
         currentRow.push(currentNode);
       }
       nodes.push(currentRow);
     }
     this.setState({ nodes });
+
+    // TODO: Add event listener for 'resize' to resize board
+    // https://stackoverflow.com/questions/19014250/rerender-view-on-browser-resize-with-react
   }
 
   render() {
@@ -44,24 +46,22 @@ export default class PathfindingVisualizer extends Component {
 
     return (
       <table className="board">
-        <tbody>
-          {nodes.map((row, rowIdx) => {
-            return (
-              <tr key={rowIdx}>
-                {row.map((node, nodeIdx) => {
-                  const { isStart, isFinish } = node;
-                  return (
-                    <Node
-                      key={nodeIdx}
-                      isStart={isStart}
-                      isFinish={isFinish}
-                    ></Node>
-                  );
-                })}
-              </tr>
-            );
-          })}
-        </tbody>
+        {nodes.map((row, rowIdx) => {
+          return (
+            <tr key={rowIdx}>
+              {row.map((node, nodeIdx) => {
+                const { isStart, isFinish } = node;
+                return (
+                  <Node
+                    key={nodeIdx}
+                    isStart={isStart}
+                    isFinish={isFinish}
+                  ></Node>
+                );
+              })}
+            </tr>
+          );
+        })}
       </table>
     );
   }
