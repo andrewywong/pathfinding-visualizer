f626cc4d493327c07d6e17222208b4bd03e447b6
andrewywong <ndrew.wong@gmail.com>
2020-08-08 18:16:59 -0500

    Add timer functions to pathfinder class

--- src/algorithms/Pathfinder.js
+++ src/algorithms/Pathfinder.js
@@ -1,3 +1,28 @@
+import Timer from './Timer';
 export default class Pathfinder {
-  constructor() {}
+  constructor(start, finish, board, updateNodeVisited, updateNodeShortest) {
+    this.start = start;
+    this.finish = finish;
+    this.updateNodeVisited = updateNodeVisited;
+    this.updateNodeShortest = updateNodeShortest;
+    this.board = board;
+    this.timers = [];
+  }
+
+  clearTimers() {
+    this.timers.forEach((timer) => {
+      timer.clear();
+    });
+    this.timers = [];
+  }
+
+  pauseTimers() {
+    this.timers.forEach((timer) => timer.pause());
+  }
+
+  resumeTimers() {
+    this.timers.forEach((timer) => timer.resume());
+  }
+
+  drawShortestPath = () => {};
 }
